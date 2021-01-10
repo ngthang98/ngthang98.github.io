f438f1e69229f27e11e7779bfd7ea9e39de8a521
ngthang98 <75118447+ngthang98@users.noreply.github.com>
2021-01-11 06:16:23 +0700

    bai tap buoi 17

---
+++ bai tap Alert/scripts.js
@@ -0,0 +1,10 @@
+// let button = document.querySelector(".button");
+// alert = document.querySelector(".alert");
+// button.onclick = function () {
+//     let text = document.createElement("detail");
+//     text = "I'M DEPRESSING";
+//     alert.append(text);
+//     let time = setInterval(function () {
+//         alert.removeChild(text)
+//     }, 3000)
+// }
